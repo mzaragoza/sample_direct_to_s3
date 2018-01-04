811f1e0772c965bb9541648d2eca8b3e7b3bca53
Moises Zaragoza <mZaragoza@circletechfl.com>
2018-01-04 09:16:54 -0500

    scaffold user

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module SampleDirectToS3
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.before_initialize do
+      dev = File.join(Rails.root, 'config', 'config.yml')
+      YAML.load(File.open(dev)).each do |key,value|
+      ENV[key.to_s] = value
+      end if File.exists?(dev)
+    end
   end
 end
